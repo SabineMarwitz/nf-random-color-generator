9b0da49e6df9d376bf37e127b8233b365686e826
SabineMarwitz <158596766+SabineMarwitz@users.noreply.github.com>
2025-07-09 10:51:39 +0200

    Add comments

--- src/app.ts
+++ src/app.ts
@@ -9,17 +9,19 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+// create a random rgb color and return as ColorInstance
 function getRandomColor() {
-  const hexCharacters = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+  const hexCharacters = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F",];
 
   let newColor = "#";
   for (let i = 0; i < 6; i++) {
-    const randomIndex = Math.floor( Math.random() * 16);
+    const randomIndex = Math.floor(Math.random() * 16);
     newColor += hexCharacters[randomIndex];
   }
   return Color(newColor);
-};
+}
 
+// after each request get another color in json format
 app.get("/", (req, res) => {
   const myColor = getRandomColor();
   res.json({
